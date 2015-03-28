3243f558bef2f7f34655d06fde555a0839ba1303
Taylor Lin <jorden.c13@gmail.com>
2015-03-28 21:18:39 +0000

    trying implment ajax

--- app/actions.rb
+++ app/actions.rb
@@ -1,16 +1,24 @@
 # Homepage (Root path)
 require 'google_places'
+require 'sinatra/json'
 require 'pry'
 
 get '/' do
-  @client = GooglePlaces::Client.new("AIzaSyCRKHOPOd_h4GLfqrLPqkEap7l3Q_Tuf9A")
-  
-  @spots = @client.spots(49.282130099999996, -123.10830340000001, :types => )
-
   erb :index
 end
 
-# get '/selection' do
-#   @meal = params[:meal].to_s
+post '/selection' do
+  @client = GooglePlaces::Client.new("AIzaSyCRKHOPOd_h4GLfqrLPqkEap7l3Q_Tuf9A")
   
-# end
+  @meal = params[:meal].to_s
+  @danger = params[:danger].to_i
+  @radius = params[:radius].to_i
+
+  @spots = @client.spots(49.282130099999996, -123.10830340000001, :types => @meal)
+  
+  binding.pry
+
+  # json @spots
+  json :myObj => @meal
+
+end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,20 +1,37 @@
 $(document).ready(function() {
-  // var meal, danger, radius;
+  var meal, danger, radius;
 
-  // $('.meal_section button').click(function() { 
-  //   meal = $(this).attr('value');
-  // });
+  $('.meal_section button').click(function() { 
+    meal = $(this).val();
+  });
 
-  // $('.danger_section button').click(function() { 
-  //   danger = $(this).text();
-  // });
+  $('.danger_section button').click(function() { 
+    danger = $(this).val();
+  });
 
-  // $('.radius_section button').click(function() { 
-  //   radius = $(this).text();
-  // });
+  $('.radius_section button').click(function() { 
+    radius = $(this).val();
+  });
 
-  // $('#go').click(function() {
-  //   $.get('/selection', {"meal": meal, "danger": danger, "radius": radius});
-  // });
+  $('#go').click(function() {
+    // $.get('/selection', {"meal": meal, "danger": danger, "radius": radius});
+    // window.location = somelocation
+    $.ajax({
+      url: '/selection',
+      method: 'POST',
+      data: {
+        meal: meal,
+        dnager: danger,
+        radius: radius
+      }
+    }).done(function(dataFromServer) {
+      // put what you want to do to page after response here
+      // dataFromServer.name
+      // $( "#result_section" ).load(dataFromServer.name)
+      console.log('return from sinatra', dataFromServer)
+    }).error(function(errorResponseFromServer) {
+      console.log(errorResponseFromServer)
+    })
+  });
 
 });
