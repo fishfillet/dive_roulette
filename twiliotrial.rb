b83f5fa66169e08bdfd46d614e22b66feaf35c04
Sarah Clinton-Baker <sarahclintonbaker@gmail.com>
2015-03-28 23:28:45 +0000

    images

---
+++ twiliotrial.rb
@@ -0,0 +1,12 @@
+
+require 'twilio-ruby'
+ 
+# Get your Account Sid and Auth Token from twilio.com/user/account
+account_sid = 'AC0e990bcf90c70f5500d32441ed1d3287'
+auth_token = '27a60a6d3d4a5a78edfa6bbb822c8d3f'
+@client = Twilio::REST::Client.new account_sid, auth_token
+ 
+message = @client.account.messages.create(:body => "Hi DANNNNNNIEL",
+    :to => "+16047152878",     # Replace with your phone number
+    :from => "+16042295836")   # Replace with your Twilio number
+puts message.sid
