b51b3edba11ad3d43d4b0f8c687a7bc47d1c697e
Taylor Lin <jorden.c13@gmail.com>
2015-03-29 03:47:49 +0000

    merge code

--- app/actions.rb
+++ app/actions.rb
@@ -1,5 +1,7 @@
 # Homepage (Root path)
 require 'google_places'
+require 'sinatra/json'
+require 'pry'
 
 get '/' do
   # @client = GooglePlaces::Client.new("AIzaSyCRKHOPOd_h4GLfqrLPqkEap7l3Q_Tuf9A")
@@ -7,4 +9,20 @@ get '/' do
   # @spots = @client.spots(49.282130099999996, -123.10830340000001, :types => 'restaurant')
 
   erb :index
+end
+
+post '/selection' do
+  @client = GooglePlaces::Client.new("AIzaSyCRKHOPOd_h4GLfqrLPqkEap7l3Q_Tuf9A")
+  
+  @meal = params[:meal].to_s
+  @danger = params[:danger].to_i
+  @radius = params[:radius].to_i
+
+  @spots = @client.spots(49.282130099999996, -123.10830340000001, :type => @meal)
+
+  puts @spots.each do |spot|
+    stop
+  end
+  
+  # json @spots
 end
--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,66 +1,92 @@
 $(document).ready(function() {
 
-// Page loads, delay to show logo, scrolls to first section.
-$("body").delay(1000).animate({ scrollTop: 1000 }, "slow");
+  // Page loads, delay to show logo, scrolls to first section.
+  $("body").delay(1000).animate({ scrollTop: 1000 }, "slow");
 
-var $obj = {
-  "food_type": "breakfast"
+  var $obj = {
+    "food_type": "breakfast"
+  };
 
-};
+  $("body").on("click", ".food_type", function(event) {
+    var d_food_type = $(this).attr('d-food-type');
 
-$("body").on("click", ".food_type", function(event) {
-  console.log("Clicked the food-type button!");
-  var d_food_type = $(this).attr('d-food-type');
-  console.log(d_food_type);
+    $('.food_type').attr('value', d_food_type);
+    $('html, body').animate({                   //smooth scrollin bud.
+      scrollTop: $("#danger_section").offset().top
+    }, 1000);
+    //obj.food_type = ;
+    // $('#food_type').val(d_food_type);
+  });
 
-  $('.food_type').attr('value', d_food_type);
-  $('html, body').animate({                   //smooth scrollin bud.
-    scrollTop: $("#page3").offset().top
-  }, 1000);
-  //obj.food_type = ;
-  // $('#food_type').val(d_food_type);
-  console.log($('#food_type').val());
-});
+  $("body").on("click", ".danger_type", function(event) {
+    var d_danger_type = $(this).attr('d-danger-type');
 
-$("body").on("click", ".danger_type", function(event) {
-  console.log("Clicked the danger-type button!");
-  var d_danger_type = $(this).attr('d-danger-type');
-  console.log(d_danger_type);
+    $('.danger_type').attr('value', d_danger_type);
+    $('html, body').animate({                   //smooth scrollin bud.
+      scrollTop: $("#radius_section").offset().top
+    }, 1000);
+    //obj.food_type = ;
+    // $('#food_type').val(d_food_type);
+  });
 
-  $('.danger_type').attr('value', d_danger_type);
-  $('html, body').animate({                   //smooth scrollin bud.
-    scrollTop: $("#page4").offset().top
-  }, 1000);
-  //obj.food_type = ;
-  // $('#food_type').val(d_food_type);
-  console.log($('#danger_type').val());
-});
+  $("body").on("click", ".distance", function(event) {
+    var d_distance = $(this).attr('d-distance');
 
-$("body").on("click", ".distance", function(event) {
-  console.log("Clicked the distance button!");
-  var d_distance = $(this).attr('d-distance');
-  console.log(d_distance);
+    $('.distance').attr('value', d_distance);
+    $('html, body').animate({                   //smooth scrollin bud.
+      scrollTop: $("#go_section").offset().top
+    }, 1000);
+    //obj.food_type = ;
+    // $('#food_type').val(d_food_type);
+  });
 
-  $('.distance').attr('value', d_distance);
-  $('html, body').animate({                   //smooth scrollin bud.
-    scrollTop: $("#page5").offset().top
-  }, 1000);
-  //obj.food_type = ;
-  // $('#food_type').val(d_food_type);
-  console.log($('#distance').val());
-});
-// $("body").on("click", "#submitttt", function(event) {
-//   $.getJSON("/test"[, $obj][, function]( data ) {
-//     console.log("got data!");
-//     // debugger;
-//   });
-// });
+  $("body").on("click", ".go", function(event) {
+    var d_distance = $(this).attr('d-distance');
 
-// $("#food_type").click(function() {
-//     $('html, body').animate({
-//         scrollTop: $("#myDiv").offset().top
-//     }, 2000);
-// });
+    $('.distance').attr('value', d_distance);
+    $('html, body').animate({                   //smooth scrollin bud.
+      scrollTop: $("#result_section").offset().top
+    }, 1000);
+    //obj.food_type = ;
+    // $('#food_type').val(d_food_type);
+  });
 
-  // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
+  var meal, danger, radius;
+
+  $('#meal_section button').click(function() { 
+    meal = $(this).val();
+  });
+
+  $('#danger_section button').click(function() { 
+    danger = $(this).val();
+  });
+
+  $('#radius_section button').click(function() { 
+    radius = $(this).val();
+  });
+
+  $('#go').click(function() {
+    // $.get('/selection', {"meal": meal, "danger": danger, "radius": radius});
+    // window.location = somelocation
+    $.ajax({
+      url: '/selection',
+      method: 'POST',
+      data: {
+        meal: meal,
+        dnager: danger,
+        radius: radius
+      }
+    }).done(function(dataFromServer) {
+      name = dataFromServer[0].name
+      address = dataFromServer[0].address
+      // name = "it works"
+      // $( "#result_section" ).load(dataFromServer.name)
+      // console.log('return from sinatra', dataFromServer)
+      $( "#result_section" ).append( "<h1>" + name + "</h1>" );
+      $( "#result_section" ).append( "<h1>" + address + "</h1>" );
+    }).error(function(errorResponseFromServer) {
+      console.log(errorResponseFromServer)
+    })
+  });
+
 });
