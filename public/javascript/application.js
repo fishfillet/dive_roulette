f9431392a57c7fb59ab886c030b1bedc2f3d7503
Taylor Lin <jorden.c13@gmail.com>
2015-03-29 02:13:51 +0000

    mannual copy some files from feature3

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -36,4 +36,39 @@ $(document).ready(function() {
     })
   });
 
+  var $obj = {
+    "food_type": "breakfast"
+
+  };
+
+
+  $("body").on("click", ".food_type", function(event) {
+      console.log("Clicked the food-type button!");
+
+    var d_food_type = $(this).attr('d-food-type');
+    console.log(d_food_type);
+
+    $('#food_type').attr('value', d_food_type);
+    //obj.food_type = ;
+    // $('#food_type').val(d_food_type);
+
+
+    console.log($('#food_type').val());
+  });
+
+
+  $("body").on("click", "#submitttt", function(event) {
+    $.getJSON("/test"[, $obj][, function]( data ) {
+      console.log("got data!");
+      // debugger;
+
+    });
+  });
+
 });
+
+
+
+
+
+
