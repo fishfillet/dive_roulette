c3079416145df3ceacefa4dce32dcfdf80697819
Sarah Clinton-Baker <sarahclintonbaker@gmail.com>
2015-03-29 01:49:08 +0000

    added more buttons

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -1,37 +1,66 @@
 $(document).ready(function() {
 
+// Page loads, delay to show logo, scrolls to first section.
+$("body").delay(1000).animate({ scrollTop: 1000 }, "slow");
 
 var $obj = {
   "food_type": "breakfast"
 
 };
 
-
 $("body").on("click", ".food_type", function(event) {
-    console.log("Clicked the food-type button!");
-
+  console.log("Clicked the food-type button!");
   var d_food_type = $(this).attr('d-food-type');
   console.log(d_food_type);
 
-  $('#food_type').attr('value', d_food_type);
+  $('.food_type').attr('value', d_food_type);
+  $('html, body').animate({                   //smooth scrollin bud.
+    scrollTop: $("#page3").offset().top
+  }, 1000);
   //obj.food_type = ;
   // $('#food_type').val(d_food_type);
-
-
   console.log($('#food_type').val());
 });
 
+$("body").on("click", ".danger_type", function(event) {
+  console.log("Clicked the danger-type button!");
+  var d_danger_type = $(this).attr('d-danger-type');
+  console.log(d_danger_type);
 
-$("body").on("click", "#submitttt", function(event) {
-  $.getJSON("/test"[, $obj][, function]( data ) {
-    console.log("got data!");
-    // debugger;
-
-  });
+  $('.danger_type').attr('value', d_danger_type);
+  $('html, body').animate({                   //smooth scrollin bud.
+    scrollTop: $("#page4").offset().top
+  }, 1000);
+  //obj.food_type = ;
+  // $('#food_type').val(d_food_type);
+  console.log($('#danger_type').val());
 });
 
+$("body").on("click", ".distance", function(event) {
+  console.log("Clicked the distance button!");
+  var d_distance = $(this).attr('d-distance');
+  console.log(d_distance);
 
+  $('.distance').attr('value', d_distance);
+  $('html, body').animate({                   //smooth scrollin bud.
+    scrollTop: $("#page5").offset().top
+  }, 1000);
+  //obj.food_type = ;
+  // $('#food_type').val(d_food_type);
+  console.log($('#distance').val());
+});
+// $("body").on("click", "#submitttt", function(event) {
+//   $.getJSON("/test"[, $obj][, function]( data ) {
+//     console.log("got data!");
+//     // debugger;
+//   });
+// });
 
+// $("#food_type").click(function() {
+//     $('html, body').animate({
+//         scrollTop: $("#myDiv").offset().top
+//     }, 2000);
+// });
 
   // See: http://docs.jquery.com/Tutorials:Introducing_$(document).ready()
 });
