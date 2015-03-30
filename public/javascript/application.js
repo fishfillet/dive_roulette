6fbbc35df8203fc408e8988564ac8ffc177a3cd8
Sarah Clinton-Baker <sarahclintonbaker@gmail.com>
2015-03-30 15:51:25 +0000

    updated map features

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -70,6 +70,28 @@ $(document).ready(function() {
     // $( "h1.address" ).replaceWith( "<h1 class="address">" + address + "</h1>" );
 
   // map stuff starts
+  //   var styles = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
+
+  //   var styledMap = new google.maps.StyledMapType(styles,
+  //   {name: "Styled Map"});
+
+  //     // Create a map object, and include the MapTypeId to add
+  // // to the map type control.
+  // var mapOptions = {
+  //   zoom: 11,
+  //   center: new google.maps.LatLng(55.6468, 37.581),
+  //   mapTypeControlOptions: {
+  //     mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+  //   }
+  // };
+  // var map = new google.maps.Map(document.getElementById('map-canvas'),
+  //   mapOptions);
+
+  // //Associate the styled map with the MapTypeId and set it to display.
+  // map.mapTypes.set('map_style', styledMap);
+  // map.setMapTypeId('map_style');
+
+
     console.log("LatLng", lat, lng);
     var centerPoint = new google.maps.LatLng(lat, lng);
     var mapOptions = {
@@ -86,6 +108,7 @@ $(document).ready(function() {
       animation: google.maps.Animation.DROP
 
   });
+
     
   }
   // test for map ------------------------------
