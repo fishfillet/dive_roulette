77dde995d66b8f7f548aeb4ad8567a660a3a6732
fishfillet <daniel.susetyo@gmail.com>
2015-04-11 00:02:21 +0000

    add style to map

--- public/javascript/application.js
+++ public/javascript/application.js
@@ -56,6 +56,10 @@ $(document).ready(function() {
   });
 
   var map;
+  var styles = [{"featureType":"landscape","stylers":[{"saturation":-100},{"lightness":65},{"visibility":"on"}]},{"featureType":"poi","stylers":[{"saturation":-100},{"lightness":51},{"visibility":"simplified"}]},{"featureType":"road.highway","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"road.arterial","stylers":[{"saturation":-100},{"lightness":30},{"visibility":"on"}]},{"featureType":"road.local","stylers":[{"saturation":-100},{"lightness":40},{"visibility":"on"}]},{"featureType":"transit","stylers":[{"saturation":-100},{"visibility":"simplified"}]},{"featureType":"administrative.province","stylers":[{"visibility":"off"}]},{"featureType":"water","elementType":"labels","stylers":[{"visibility":"on"},{"lightness":-25},{"saturation":-100}]},{"featureType":"water","elementType":"geometry","stylers":[{"hue":"#ffff00"},{"lightness":-25},{"saturation":-97}]}]
+
+  var styledMap = new google.maps.StyledMapType(styles,
+    {name: "Styled Map"});
 
   function drawCenteredMap(dataFromServer) {
     console.log("datafromservre", dataFromServer)
@@ -74,16 +78,22 @@ $(document).ready(function() {
     var centerPoint = new google.maps.LatLng(lat, lng);
     var mapOptions = {
       zoom: 15,
-      center: centerPoint
+      center: centerPoint,
+      mapTypeControlOptions: {
+        mapTypeIds: [google.maps.MapTypeId.ROADMAP, 'map_style']
+    }
     };
 
     map = new google.maps.Map(document.getElementById('map-canvas'), mapOptions);
       
-      var marker = new google.maps.Marker({
-      position: centerPoint,
-      map: map,
-      animation: google.maps.Animation.DROP
-  });
+    var marker = new google.maps.Marker({
+    position: centerPoint,
+    map: map,
+    animation: google.maps.Animation.DROP
+    });
+
+    map.mapTypes.set('map_style', styledMap);
+    map.setMapTypeId('map_style');
     
   }//end of drawCenteredMap
   // test for map ------------------------------
